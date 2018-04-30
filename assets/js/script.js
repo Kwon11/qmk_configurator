b10ccfe1ed9cd44f0eb840cf9698031465f366fa
Yan-Fa Li <yanfali@gmail.com>
2018-04-29 21:58:41 -0700

    experiment with tabs

--- assets/js/script.js
+++ assets/js/script.js
@@ -53,9 +53,10 @@ $(document).ready(() => {
   var keycodes = getKeycodes();
   $(window).on('hashchange', urlRouteChanged);
 
-  $.each(keycodes, createKeyCodeUI);
-
   var $keycodes = $('.keycode'); // wait until they are created
+  keycodes.reduce(createKeyCodeUI, { $el: $keycodes, $curEl: $keycodes, count: 1 });
+  $keycodes.tabs();
+
   $keycodes.each(makeDraggable);
 
   // click to assign keys to keymap
@@ -486,8 +487,14 @@ $(document).ready(() => {
     });
   }
 
-  function createKeyCodeUI(k, d) {
-    if (d.code) {
+  function createKeyCodeUI(acc, d) {
+    if (d.tab) {
+      var $ul = acc.$el.find('ul');
+      $ul.append('li').append('a href="#tabs-' + acc.count).html(d.tab);
+      var $newTab = acc.$el.append('div id="#tabs-' + acc.count);
+      acc.count += 1;
+      acc.curEl = $newTab;
+    } else if (d.code) {
       var keycode = $('<div>', {
         class: 'keycode keycode-' + d.width + ' keycode-' + d.type,
         'data-code': d.code,
@@ -495,15 +502,16 @@ $(document).ready(() => {
         html: d.name,
         title: d.title
       });
-      $('#keycodes').append(keycode);
+      acc.$curEl.append(keycode);
     } else {
-      $('#keycodes').append(
+      acc.$curEl.append(
         $('<div>', {
           class: 'space space-' + d.width,
           html: d.label
         })
       );
     }
+    return acc;
   }
 
   function urlRouteChanged() {
@@ -1019,8 +1027,152 @@ $(document).ready(() => {
     }
   }
 
+  // encapsulate the keymap
+  function Keymap() {
+    var instance = this;
+    instance.km = [];
+    instance.l = 0;
+    instance.dirty = false;
+
+    _.extend(this, {
+      assignKey: assignKey,
+      changeLayer: _changeLayer,
+      clear: clear,
+      clearDirty: clearDirty,
+      exportLayers: exportLayers,
+      getKey: getKey,
+      initLayer: initLayer,
+      isDirty: isDirty,
+      setContents: setContents,
+      setDirty: setDirty,
+      setKey: setKey,
+      setKeycodeLayer: setKeycodeLayer,
+      setText: setText,
+      size: size,
+      swapKeys: swapKeys
+    });
+    return instance;
+
+    //////////
+    // Impl
+    //////////
+
+    function assignKey(__layer, index, name, code, type) {
+      instance.km[__layer][index] = {
+        name: name,
+        code: code,
+        type: type
+      };
+      var keycode = instance.km[__layer][index];
+      if (keycode.type === 'layer') {
+        instance.km[__layer][index].layer = 0;
+      }
+      return keycode;
+    }
+
+    function setContents(index, key) {
+      instance.km[instance.l][index].contents = key;
+    }
+
+    function _changeLayer(newLayer) {
+      instance.l = newLayer;
+    }
+
+    function clear() {
+      instance.km = [];
+    }
+
+    function initLayer(__layer) {
+      instance.km[__layer] = {};
+    }
+
+    function setKey(__layer, index, key) {
+      instance.km[__layer][index] = key;
+      return instance.km[__layer][index];
+    }
+
+    function size(__layer) {
+      return _.size(instance.km[__layer]);
+    }
+
+    function getKey(__layer, index) {
+      if (instance.km[__layer] === undefined) {
+        instance.km[__layer] = {};
+      }
+      return instance.km[__layer][index];
+    }
+
+    function swapKeys(__layer, srcIndex, dstIndex) {
+      var temp = instance.km[__layer][srcIndex];
+      instance.km[__layer][srcIndex] = instance.km[__layer][dstIndex];
+      instance.km[__layer][dstIndex] = temp;
+      instance.dirty = true;
+    }
+
+    function setText(__layer, index, text) {
+      instance.km[__layer][index].text = text;
+    }
+
+    function exportLayers() {
+      return _.reduce(
+        instance.km,
+        function(layers, _layer, k) {
+          layers[k] = [];
+          var aLayer = _.reduce(
+            _layer,
+            function(acc, key) {
+              var keycode = key.code;
+              if (key.code.indexOf('(kc)') !== -1) {
+                if (key.contents) {
+                  keycode = keycode.replace('kc', key.contents.code);
+                } else {
+                  keycode = keycode.replace('kc', 'KC_NO');
+                }
+              }
+              if (key.code.indexOf('(layer)') !== -1) {
+                keycode = keycode.replace('layer', key.layer);
+              }
+              if (key.code.indexOf('text') !== -1) {
+                keycode = key.text;
+              }
+              acc.push(keycode);
+              return acc;
+            },
+            []
+          );
+          layers[k] = aLayer;
+          return layers;
+        },
+        []
+      );
+    }
+
+    function setKeycodeLayer(_layer, index, toLayer) {
+      instance.km[_layer][index].layer = toLayer;
+      if (toLayer !== _layer) {
+        if (instance.km[toLayer] === undefined) {
+          instance.km[toLayer] = {};
+        }
+        instance.km[toLayer][index] = { name: '▽', code: 'KC_TRNS' };
+      }
+    }
+
+    function isDirty() {
+      return instance.dirty;
+    }
+
+    function clearDirty() {
+      instance.dirty = false;
+    }
+
+    function setDirty() {
+      instance.dirty = true;
+    }
+  }
+
   function getKeycodes() {
     return [
+      { tab: 'Alphas and Mods' },
       { name: 'Esc', code: 'KC_ESC', keys: 'esc' },
       { width: 1000 },
       { name: 'F1', code: 'KC_F1' },
@@ -1496,148 +1648,5 @@ $(document).ready(() => {
       { name: 'Media Stop', code: 'KC_MSTP', title: 'Media Stop' },
       { name: 'Play', code: 'KC_MPLY', title: 'Play/Pause' }
     ];
-  }
-
-  // encapsulate the keymap
-  function Keymap() {
-    var instance = this;
-    instance.km = [];
-    instance.l = 0;
-    instance.dirty = false;
-
-    _.extend(this, {
-      assignKey: assignKey,
-      changeLayer: _changeLayer,
-      clear: clear,
-      clearDirty: clearDirty,
-      exportLayers: exportLayers,
-      getKey: getKey,
-      initLayer: initLayer,
-      isDirty: isDirty,
-      setContents: setContents,
-      setDirty: setDirty,
-      setKey: setKey,
-      setKeycodeLayer: setKeycodeLayer,
-      setText: setText,
-      size: size,
-      swapKeys: swapKeys
-    });
-    return instance;
-
-    //////////
-    // Impl
-    //////////
-
-    function assignKey(__layer, index, name, code, type) {
-      instance.km[__layer][index] = {
-        name: name,
-        code: code,
-        type: type
-      };
-      var keycode = instance.km[__layer][index];
-      if (keycode.type === 'layer') {
-        instance.km[__layer][index].layer = 0;
-      }
-      return keycode;
-    }
-
-    function setContents(index, key) {
-      instance.km[instance.l][index].contents = key;
-    }
-
-    function _changeLayer(newLayer) {
-      instance.l = newLayer;
-    }
-
-    function clear() {
-      instance.km = [];
-    }
-
-    function initLayer(__layer) {
-      instance.km[__layer] = {};
-    }
-
-    function setKey(__layer, index, key) {
-      instance.km[__layer][index] = key;
-      return instance.km[__layer][index];
-    }
-
-    function size(__layer) {
-      return _.size(instance.km[__layer]);
-    }
-
-    function getKey(__layer, index) {
-      if (instance.km[__layer] === undefined) {
-        instance.km[__layer] = {};
-      }
-      return instance.km[__layer][index];
-    }
-
-    function swapKeys(__layer, srcIndex, dstIndex) {
-      var temp = instance.km[__layer][srcIndex];
-      instance.km[__layer][srcIndex] = instance.km[__layer][dstIndex];
-      instance.km[__layer][dstIndex] = temp;
-      instance.dirty = true;
-    }
-
-    function setText(__layer, index, text) {
-      instance.km[__layer][index].text = text;
-    }
-
-    function exportLayers() {
-      return _.reduce(
-        instance.km,
-        function(layers, _layer, k) {
-          layers[k] = [];
-          var aLayer = _.reduce(
-            _layer,
-            function(acc, key) {
-              var keycode = key.code;
-              if (key.code.indexOf('(kc)') !== -1) {
-                if (key.contents) {
-                  keycode = keycode.replace('kc', key.contents.code);
-                } else {
-                  keycode = keycode.replace('kc', 'KC_NO');
-                }
-              }
-              if (key.code.indexOf('(layer)') !== -1) {
-                keycode = keycode.replace('layer', key.layer);
-              }
-              if (key.code.indexOf('text') !== -1) {
-                keycode = key.text;
-              }
-              acc.push(keycode);
-              return acc;
-            },
-            []
-          );
-          layers[k] = aLayer;
-          return layers;
-        },
-        []
-      );
-    }
-
-    function setKeycodeLayer(_layer, index, toLayer) {
-      instance.km[_layer][index].layer = toLayer;
-      if (toLayer !== _layer) {
-        if (instance.km[toLayer] === undefined) {
-          instance.km[toLayer] = {};
-        }
-        instance.km[toLayer][index] = { name: '▽', code: 'KC_TRNS' };
-      }
-    }
-
-    function isDirty() {
-      return instance.dirty;
-    }
-
-    function clearDirty() {
-      instance.dirty = false;
-    }
-
-    function setDirty() {
-      instance.dirty = true;
-    }
   }
 });
